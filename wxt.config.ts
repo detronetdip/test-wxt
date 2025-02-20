fba8338f0dbc4380dd9b7681ad9dc29e9c387836
Ayondip <ayondip.jana@scalereal.com>
2025-02-20 16:30:39 +0530

    update

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,4 +4,13 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
+  outDir: 'dist',
+  manifest: () => ({
+    name: 'Fravity: AI co-pilot',
+    permissions: ['storage'],
+    key: import.meta.env.WXT_CRX_KEY,
+  }),
+  runner: {
+    chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
+  }
 });
